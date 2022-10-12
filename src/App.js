5a5c50ac8ce32fb88922d92cfd4eb11aad2c4127
Pradeep Yadav <pradeep.yadav@ticketmaster.com>
2022-10-12 19:36:15 +0530

    Update App.js
    Adding key

--- src/App.js
+++ src/App.js
@@ -10,12 +10,15 @@ class App extends Component {
       monsters: [
         {
           name: "Pradeep",
+          id:"1acwssc"
         },
         {
           name: "jack",
+          id:"2acwssc"
         },
         {
           name: "raj",
+          id:"3acwssc"
         },
       ],
     };
@@ -25,7 +28,7 @@ class App extends Component {
     return (
       <div className="App">
         {this.state.monsters.map((monster) => {
-          return (<h1> {monster.name}</h1>)
+          return  <div key={ monster.id}>  <h1> {monster.name}</h1></div>;
 
         })}
       </div>
