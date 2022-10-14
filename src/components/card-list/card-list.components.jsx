3f71bb4d31cff609b9930fa8c03d799727534a3f
Pradeep Yadav <pradeep.yadav@ticketmaster.com>
2022-10-14 19:28:18 +0530

    add imaage to card box and css

--- src/components/card-list/card-list.components.jsx
+++ src/components/card-list/card-list.components.jsx
@@ -1,12 +1,21 @@
 import { Component } from "react";
+import { scryRenderedComponentsWithType } from "react-dom/test-utils";
+import "./card-list.styles.css"
 
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
     return (
-        <div>
+        <div className="card-list">
            {monsters.map((monster) =>{
-             return  <h1 key={monster.id }>{monster.name}</h1>
+            const { id,name, email }= monster;
+             return(
+                <div className="card-container" key={id}>
+                    <img alt={`Monster ${monster.name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`} ></img>
+                    <h1>{name}</h1>
+                    <p>{email}</p>
+                </div>
+             )
            })}
         </div>
     );
