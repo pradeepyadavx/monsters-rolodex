6ddee8ed72f813875ee1f0577ab5e52a7b1d7b6c
Pradeep Yadav <pradeep.yadav@ticketmaster.com>
2022-10-14 13:26:59 +0530

    Update App.js
    add filter

--- src/App.js
+++ src/App.js
@@ -29,10 +29,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search monsters"
+          onChange={(event) => {
+            const searchString=event.target.value.toLocaleLowerCase();
+            const filterdMonster = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+            this.setState(() => {
+              return { monsters: filterdMonster };
+            });
+          }}
+        ></input>
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
-              {" "}
               <h1> {monster.name}</h1>
             </div>
           );
